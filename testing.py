aa2d733a20e52030eba8b7161c64de1a81fcaf0b
AnassB1 <79224460+Ananas1Anass@users.noreply.github.com>
2023-01-23 16:16:49 +0100

    Add files via upload

--- testing.py
+++ testing.py
@@ -12,12 +12,13 @@ from utils import load_checkpoint
 from PIL import Image
 from utils import predict_image
 
+
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 # parameters
 IMAGE_HEIGHT = 256
 IMAGE_WIDTH = 256
 
 model = UNET(in_channels=3, out_channels=1).to(DEVICE)
-load_checkpoint(torch.load("my_checkpoint_updated.pth.tar", map_location=torch.device('cpu')), model)
+load_checkpoint(torch.load("my_checkpoint_updated.pth.tar", map_location=torch.device(DEVICE)), model)
 img = Image.open('Anass.jpeg')
 predict_image(img, model, device=DEVICE)
